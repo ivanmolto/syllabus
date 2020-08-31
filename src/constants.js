bd198fb94217724c4ba07a9396af7c6720d28fe8
Ivan Molto <ivanmolto@gmail.com>
2020-08-31 12:09:52 +0200

    chore: Modify package

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const APP_NAME = "Syllabus-alpha-4";
+export const APP_NAME = "Syllabus-beta-1";
 export const TX_PATTERN = /[A-Za-z0-9-_]{43}/;
 export const CONTRACTID = "TmeTF0zp4YEHL_1vgM9K3wrxv3tjjckjO6DcOI_Drfs";
