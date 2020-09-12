b19d344d1e40f4a39ee16a612209653c6f7e75f8
Ivan Molto <ivanmolto@gmail.com>
2020-09-12 16:05:39 +0200

    chore: Clean postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,12 @@
 const tailwindcss = require("tailwindcss");
-
 const purgecss = require("@fullhuman/postcss-purgecss")({
   content: ["./src/**/*.svelte", "./public/**/*.html"],
   defaultExtractor: content => {
     const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
     const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
     return broadMatches.concat(innerMatches)
-    //content.match(/[A-Za-z0-9-_:/]+/g) || []
   }
 });
-
 module.exports = {
   plugins: [
     tailwindcss("./tailwind.js"),
