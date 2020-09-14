b0b710daa0d6b4710816a3ad9da4e705a16d0a2b
Ivan Molto <ivanmolto@gmail.com>
2020-09-14 10:03:36 +0200

    chore: Migrate to new Community Contract version

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const APP_NAME = "Syllabus-beta-1";
+export const APP_NAME = "Syllabus-beta-2";
 export const TX_PATTERN = /[A-Za-z0-9-_]{43}/;
-export const CONTRACTID = "TmeTF0zp4YEHL_1vgM9K3wrxv3tjjckjO6DcOI_Drfs";
+export const CONTRACTID = "20dF4g9NhWvzat5A58Kbe2uF1GaBmIcVCDYDRi6XeJs";
